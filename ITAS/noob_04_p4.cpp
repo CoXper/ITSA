4835acde8fe37c042d4d8fd4339d351d831113f8
CoXper <a412410009@alum.ccu.edu.tw>
2024-12-02 13:01:21 +0800

    AC

---
+++ ITAS/noob_04_p4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n] = {0}, prefix[n] = {0};
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if(i == 0) prefix[i] = arr[i];
+        else prefix[i] = prefix[i-1] + arr[i]; //前綴和
+    }
+
+    for(int i = 0; i < n-1; i++)
+        cout << prefix[i] << " ";
+    
+    cout << prefix[n-1] << "\n";
+}
